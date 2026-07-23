f4b049e0ecd4ffb878efcf279cbf4c3101cd47b1
Shadow-1002 <davidmatthewwang@gmail.com>
2026-07-24 09:35:54 +1000

    Update script.js
    test bubbles and left right side texts

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 const db = firebase.database();
-
 function sendMessage() {
   const text = document.getElementById("input").value;
 
   db.ref("messages").push({
-    text: text,
+    text: "ME: " + text,
     time: Date.now()
   });
 
   document.getElementById("input").value = "";
 }
 
+
 db.ref("messages").on("child_added", snapshot => {
   const msg = snapshot.val();
 
   const div = document.createElement("div");
-  div.textContent = msg.text;
+  div.classList.add("bubble");
+
+  // If the message is yours
+  if (msg.text.startsWith("ME: ")) {
+    div.classList.add("me");
+    div.textContent = msg.text.replace("ME: ", "");
+  } else {
+    div.classList.add("other");
+    div.textContent = msg.text;
+  }
 
   document.getElementById("messages").appendChild(div);
 });
+
