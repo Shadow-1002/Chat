a26a0becae5c12c7c708c28f3f2bd484208ba9cc
Shadow-1002 <davidmatthewwang@gmail.com>
2026-07-25 18:32:41 +1000

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -3,19 +3,19 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  const firebaseConfig = {
-      apiKey: "AIzaSyDbcH_OAGxFrfxPRarAnY4PUdAdz-rjymE",
-      authDomain: "chat-ad084.firebaseapp.com",
-      databaseURL: "https://chat-ad084-default-rtdb.firebaseio.com",
-      projectId: "chat-ad084",
-      storageBucket: "chat-ad084.firebasestorage.app",
-      messagingSenderId: "703998786296",
-      appId: "1:703998786296:web:99cabe960b1b00dd1d990d",
-      measurementId: "G-MEVR8MCS49"
-    };
+  apiKey: "AIzaSyDbcH_OAGxFrfxPRarAnY4PUdAdz-rjymE",
+  authDomain: "chat-ad084.firebaseapp.com",
+  databaseURL: "https://chat-ad084-default-rtdb.firebaseio.com",
+  projectId: "chat-ad084",
+  storageBucket: "chat-ad084.firebasestorage.app",
+  messagingSenderId: "703998786296",
+  appId: "1:703998786296:web:99cabe960b1b00dd1d990d",
+  measurementId: "G-MEVR8MCS49"
+};
 
-    firebase.initializeApp(firebaseConfig);
+// Initialize Firebase (v9 syntax)
+const app = initializeApp(firebaseConfig);
 
+// Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
